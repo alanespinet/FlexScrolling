3d555ff17eff2735fa610fe1cc22d3c756d6537f
alanespinet <aelluvetaa@gmail.com>
2016-09-25 17:39:40 -0400

    v1

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
-$('#home').on('click', function(){
+var scrheight = $(window).innerHeight();
+var finaltop = 0;
 
+$('#menu li').on('click', function(){
+  $('#menu li').removeClass('selected');
+  $(this).addClass('selected');
+});
+
+$('#home-item').on('click', function(){
+  $('#container').css('top', '0');
 });
 
 
-$('#unreal').on('click', function(){
-
+$('#unreal-item').on('click', function(){
+  finaltop = (scrheight * -1) + 'px';
+  $('#container').css('top', finaltop);
 });
 
+
+$('#unity-item').on('click', function(){
+  finaltop = (scrheight * -2) + 'px';
+  $('#container').css('top', finaltop);
+});
+
+
+$('#cryengine-item').on('click', function(){
+  finaltop = (scrheight * -3) + 'px';
+  $('#container').css('top', finaltop);
+});
+
+
+$('#valve-item').on('click', function(){
+  finaltop = (scrheight * -4) + 'px';
+  $('#container').css('top', finaltop);
+});
